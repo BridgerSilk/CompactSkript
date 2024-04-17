777a26d7b223d39ea8eedb9163fcb4fa83670d7d
BridgerSilk <foramittik@gmail.com>
2024-04-17 09:33:02 +0200

    first commit

---
+++ compiler.py
@@ -0,0 +1,69 @@
+import os
+import re
+from syntax_mappings import mappings_syntax_event_values
+from syntax_mappings import mappings_syntax_effects
+from syntax_mappings import mappings_syntax_expressions
+from syntax_mappings import mappings_symbols
+from syntax_mappings import mappings_general
+from syntax_mappings import mappings_syntax_types
+from syntax_mappings import mappings_syntax_events
+from syntax_mappings import mappings_syntax_math
+import math
+
+# Checks if there are spaces or nothing around a word:
+# content = re.sub(r'(^|\s)word(\s|$)', r'\1word\2', content)
+
+def convert_syntax():
+    global content
+# Does NOT check for spaces
+    for key, value in mappings_syntax_event_values.items():
+        pattern = re.compile(rf'{re.escape(key)}')
+        content = pattern.sub(fr'{value}', content)
+    for key, value in mappings_syntax_effects.items():
+        pattern = re.compile(rf'(^|\s){re.escape(key)}(\s|$)')
+        content = pattern.sub(fr'\1{value}\2', content)
+    for key, value in mappings_syntax_expressions.items():
+        pattern = re.compile(rf'(^|\s){re.escape(key)}(\s|$)')
+        content = pattern.sub(fr'\1{value}\2', content)
+# Does NOT check for spaces
+    for key, value in mappings_symbols.items():
+        pattern = re.compile(rf'{re.escape(key)}')
+        content = pattern.sub(fr'{value}', content)
+    for key, value in mappings_general.items():
+        pattern = re.compile(rf'(^|\s){re.escape(key)}(\s|$)')
+        content = pattern.sub(fr'\1{value}\2', content)
+    for key, value in mappings_syntax_types.items():
+        pattern = re.compile(rf'(^|\s){re.escape(key)}(\s|$)')
+        content = pattern.sub(fr'\1{value}\2', content)
+    for key, value in mappings_syntax_types.items():
+        pattern = re.compile(rf'(^|\s){re.escape(key)}(\s|$)')
+        content = pattern.sub(fr'\1{value}\2', content)
+    for key, value in mappings_syntax_events.items():
+        pattern = re.compile(rf'(^|\s){re.escape(key)}(\s|$)')
+        content = pattern.sub(fr'\1{value}\2', content)
+    for key, value in mappings_syntax_math.items():
+        pattern = re.compile(rf'(^|\s){re.escape(key)}(\s|$)')
+        content = pattern.sub(fr'\1{value}\2', content)
+    
+def process_file(input_file, output_file):
+    global content
+    try:
+        with open(input_file, 'r') as csk_file:
+            content = csk_file.read()
+            convert_syntax()
+            with open(output_file, 'w') as sk_file:
+                sk_file.write(content)
+            print(f"Conversion successful: {input_file} -> {output_file}")
+    except FileNotFoundError:
+        print(f"Error: File '{input_file}' not found")
+
+if __name__ == "__main__":
+    current_directory = os.getcwd()
+    input_file_name = input("Enter the name of the .csk file: ")
+    input_file_path = os.path.join(current_directory, input_file_name)
+    if not input_file_name.endswith('.csk'):
+        print("Error: Pls give a file with .csk extension.")
+    else:
+        output_file_name = input_file_name.replace('.csk', '.sk')
+        output_file_path = os.path.join(current_directory, output_file_name)
+        process_file(input_file_path, output_file_path)
---
+++ syntax_mappings.py
@@ -0,0 +1,176 @@
+# Does NOT check for spaces
+mappings_syntax_event_values = {
+    "e-w": "event-world",
+    "e-b": "event-block",
+    "e-i": "event-item",
+    "e-iv": "event-inventory",
+    "e-is": "event-itemstack",
+    "e-pet": "event-potioneffecttype",
+    "e-l": "event-location",
+    "e-e": "event-entity",
+    "e-o": "event-object",
+    "e-cs": "event-commandsender",
+    "e-p": "event-player",
+    "e-c": "event-chunk",
+    "e-d": "event-direction",
+    "e-s": "event-slot",
+    "e-iva": "event-inventoryaction",
+    "e-ct": "event-clicktype",
+    "e-str": "event-string",
+    "e-pr": "event-projectile",
+    "e-dmgc": "event-damagecause",
+    "e-ie": "event-itementity",
+    "e-bd": "event-blockdata",
+    "e-tfr": "event-transformreason",
+    "e-exp": "event-experience",
+    "e-fwe": "event-fireworkeffect",
+    "e-ivcr": "event-inventoryclosereason",
+    "e-tpc": "event-teleportcause",
+    "e-it": "event-itemtype",
+    "e-le": "event-livingentity",
+    "e-qr": "event-quitreason",
+    "e-rg": "event-region",
+    "e-sr": "event-spawnreason",
+    "e-ts": "event-timespan",
+    "e-txt": "event-text"
+}
+
+# Checks for spaces
+mappings_syntax_effects = {
+    "s": "send",
+    "tp": "teleport",
+    "cle": "cancel event",
+    "acb": "action bar",
+    "stp": "stop",
+    "app": "apply",
+    "bml": "bone meal",
+    "ubn": "unban",
+    "bn": "ban",
+    "ipbn": "IP-ban",
+    "br": "break",
+    "bl": "block",
+    "br:": "break:",
+    "bl:": "block:"
+}
+
+# Checks for spaces
+mappings_syntax_expressions = {
+    "cmd": "command",
+    "cmd.als:": "aliases:",
+    "cmd.prm:": "permission:",
+    "cmd.prmmsg:": "permission message:",
+    "cmd.tr:": "trigger:",
+    "?txt": "<text>",
+    "?pl": "<player>",
+    "?ofpl": "<offline-player>",
+    "?num": "<number>",
+    "?txt:": "<text>:",
+    "?pl:": "<player>:",
+    "?ofpl:": "<offline-player>:",
+    "?num:": "<number>:"
+}
+
+mappings_syntax_expressions_nospace = {
+    "%1": "arg 1",
+    "%2": "arg 2",
+    "%3": "arg 3",
+    "%4": "arg 4",
+    "%5": "arg 5",
+    "%6": "arg 6",
+    "%7": "arg 7",
+    "%8": "arg 8",
+    "%9": "arg 9",
+    "%10": "arg 10"
+}
+
+# Does NOT check for spaces
+mappings_symbols = {
+    "-/": "#",
+    "^": "{",
+    "~": "}",
+    "$": "%"
+}
+
+# Checks for spaces
+mappings_general = {
+    "t": "at",
+    "//": "on",
+    "ev": "every",
+    "sec": "seconds",
+    "tk": "ticks",
+    "min": "minutes",
+    "hr": "hours",
+    "dy": "days",
+    "wk": "weeks",
+    "mo": "months",
+    "yr": "years",
+    "i": "in",
+    "f": "for",
+    "->": "to",
+    "!": "set",
+    "dt": "due to",
+    "u": "using",
+    "o.": "of",
+    "st": "set",
+    "b": "by",
+    "fm": "from",
+    "lt": "let",
+    "wt": "with",
+    "i.": "if",
+    "++": "and",
+    "ds": "does",
+    "nt": "not",
+    "el:": "else:",
+    "el": "else",
+    "st:": "set:",
+    "mk": "make",
+    "prm": "permission",
+    "hv": "have",
+    "hs": "has",
+    "int": "integer",
+    "rnd": "random",
+    "btwn": "between",
+    "i..": "is",
+    "abv": "above",
+    "blw": "below",
+    "pos": "position",
+    "wt": "wait",
+    "*a": "all",
+    "sl": "slot",
+    "gui.new": "a new chest inventory",
+    "rw": "rows",
+    "gui.title": "with name",
+    "nm": "named",
+    "lr": "lore"
+}
+
+# Checks for spaces
+mappings_syntax_types = {
+    "p": "player",
+    "p's": "player's",
+    "ps": "players",
+    "ps*": "all players"
+}
+
+# Checks for spaces
+mappings_syntax_events = {
+    "adv": "advancements",
+    "opn": "open",
+    "av": "anvil",
+    "cl": "click",
+    "g": "gui",
+    "arst": "armor stand",
+    "itr": "interact",
+    "cl": "close",
+    "dmg": "damage",
+    "avp": "anvil prepare",
+    "dmg": "damage",
+    "cn": "join",
+    "dcn": "quit"
+}
+
+# Checks for spaces
+mappings_syntax_math = {
+    "y": "true",
+    "n": "false",
+} 
